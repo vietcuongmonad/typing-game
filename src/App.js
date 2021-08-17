10f4c03f486de45e57591fc2c34248ed9ef99b96
Viet Cuong <hosyvietcuong@gmail.com>
2021-08-17 14:28:00 +0700

    Set up & Design the game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from "react"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const TIME_LIMIT = 10
+
+    const [timer, setOurTimer] = useState(TIME_LIMIT)
+    const [text, setText] = useState("")
+    const [wordCount, setWordCount] = useState(0)
+    const [flag, setFlag] = useState(false)
+    const textRef = useRef(null)
+
+    function handleChange(event) {
+        setText(event.target.value)
+    }
+
+    function count_word(word) {
+        let tmp = word.trim().split(" ")
+        tmp = tmp.filter(word => word !== "")
+        return tmp.length
+    }
+
+    function startGame() {
+        setOurTimer(TIME_LIMIT)
+        setText("")
+        setWordCount(0)
+        setFlag(true)
+        textRef.current.disabled = false
+        textRef.current.focus()
+    }
+
+    function endGame() {
+        setWordCount(count_word(text))
+        setFlag(false)
+    }
+
+    useEffect(() => {
+        if (timer && flag) {
+            setTimeout(() => setOurTimer(x => x-1), 1000)
+        } else if (timer === 0) {
+            endGame()
+        }
+    }, [timer, flag])
+
+    return (
+        <div>
+            <h1>How fast do you think and type your own paragraph?</h1>
+            <textarea
+                disabled={!flag}
+                value={text}
+                onChange={handleChange}
+                ref = {textRef}
+                />
+
+            <h3>Time remaining: {timer}</h3>
+
+            <button
+                onClick={startGame}
+                disabled={flag}>START</button>
+            <h1>Word count: {wordCount}</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
